3a81f4f34e61b06e73b7a1108d9c63a84ce2e9f0
ducjs <72987771+ducjs@users.noreply.github.com>
2024-03-23 01:51:25 +0700

    Tách hàm combine

--- userscript.js
+++ userscript.js
@@ -9,11 +9,13 @@
 // @grant        none
 // @license MIT
 // ==/UserScript==
+// Highlight hợp âm chưa hiện diện tiếp theo ở combine button
 
 let isShowLevel = false;
+let regRemoveColor = /maj|m|sus|aug|dim|7|2|4|6|9|11/g
 const CONFIG = {
     COMBINES: {
-        BG_COLOR: "rgb(100 195 255 / 15%)"
+        BG_COLOR: "rgb(100 195 255 / 26%)"
     }
 };
 
@@ -60,22 +62,40 @@ let CHORD_PROGRESSION = {
     MAJOR: [
         "2,5,1,6",
         "1,5,6,4", // QQ
+        "1,5,6,3","4,1,2,5", "6,3,4,2,5", "2,5,1",// Khi cô đơn
+        "1,3,6","4,5,1", "4,5,3,6", "2,4,5", // không cần phải hứa
+        "1,5,6,3,4,5,3,6,4,5",
         "4,3,2,1", "4,3,2,5,1", "4,3,6,2,5,1",// Lùi
         "4,3,6,2,3,6", // Lùi thứ
+        "1,4,5,1,4,1,0,5", // Nàng thơ
         "2,3,2,3,2,2,5", "1,6,2,4,3,6,2,2,5,1",  // Lần cuối
-        "1,3,6,2,5", "1,3,6,5,4,2,5", "1,3,6,5,4,3,6,2,5", //Giấc mơ có thật
+        // "1,3,6,2,5",
+         "1,3,6,5,4,2,5", "1,3,6,5,4,3,6,2,5","1,3,6,5,0", "1,3,6,5,4,1,2,5", //Giấc mơ có thật
         "1,5,6,3,4,1,4,5", "1,5,6,3,4,1,3,6,2,5", "1,5,6,3,4,1,3,6,2,5,1", "1,5,6,3,4,5,3,6,4,2,5", // Canon
         "1,3,6,3,4,2,5,4,5", "1,3,6,3,4,2,5,2,5", "1,3,6,3,4,1,2,5", // Canon 3
         "1,3,6,3,2,6,4,5", "1,3,6,3,2,6,5,6", "1,3,3,6,4,1,2,3", "1,3,3,6,4,5", // Người tình mùa đông
-        "4,5,3,6,2,5,1", "4,5,3,6,4,5,1", "",
+        "4,5,3,6", "4,5,3,6,4,5,1", "",
         "1,6,4,1", // MĐS
         "1,6,6,2,4,4", // Thằng điên,
         "1,3,6,5,1,4,2,5", "1,3,6,4,1,4,2,5",
-        "6,5,4"
+        "6,5,4",
+        "1,1,1,", "4,4,1", "2,0,1", "2,5,3,6", // Can't take,
+        "2,5,1,4", "2,3,6", "2,5,6" // Giữa đại lộ
     ],
     MINOR: [
-        "6,7,5,1", "6,7,5,1,6,7,3", "6,7,5,1,6,7,3,3", "6,7,5,1,6,5,1" // Tháng tư
-    ]
+        "6,7,5,1", "6,5,1", "6,7,3", "6,7,1", // Tháng tư
+        "1,5,1,5", "1,5,6,7,1", "1,7,1,7", "1,7,6,7,1", // Chờ người nơi ấy
+        "6,5,4,5,1",
+        "1,4,6", "1,3,4", // Em oii
+        "1,3,7,1", "1,3,4,1", "3,4,3,7,1", //Bước đến bên em,
+        "1,5,6,3", "4,1,6,5", "1,5,6,5", "1,5,6,5,6,4", "6,4,0,5", // Chỉ là không cùng nhau,
+        "1,5,4,5,1", "1,5,4,7,3", "4,5,1",// Ngày mai lấy chồng
+        "1,3,7,4", // Bốn chữ lắm,
+        "1,4,0,5", //Careless,
+        "1,5,7,4,6,3", //Hotel
+        "1,6,3,0"
+        
+    ]   
 }
 // Dùng some để biết nó loại gì
 let chordLyricDivs = [];
@@ -99,8 +119,11 @@ const initAnalyze = () => {
     let { rootNote, isMinor } = getRootNote();
     let chordProgressionMapping = isMinor ? CHORD_PROGRESSION.MINOR : CHORD_PROGRESSION.MAJOR;
     let rootMap = chordList[rootNote];
-
-    //Sliding windows
+    console.log({rootNote, rootMap})
+    let overlapMap = [];
+    let combineChordsSort = []
+    // Sliding windows
+    // Overlap
     /* Loop từng window
         Chỉ lọc lấy những nhóm hợp ấm khớp với prog, lấy kèm index
     */
@@ -111,36 +134,64 @@ const initAnalyze = () => {
         for (let y = i; y < tail; y++) {
             if (y === chordsName.length) break;
             let combineChords = chordsName.slice(i, y + 1);
-            // console.log({ combineChords })
             let combineChordLevelStr = genChordLevelCombineStr(combineChords, rootMap);
-            let combineChordsString = genChordCombineStr(combineChords, rootMap);
-            // continue;
-
             const MATCH_PROG = chordProgressionMapping.indexOf(combineChordLevelStr) >= 0;
             if (!MATCH_PROG) continue;
-            // console.log({ combineChords })
-            let chordHeadIndex = combineChords[0].index;
-            let chordTailIndex = combineChords[combineChords.length - 1].index;
-
-            let chordHeadDiv = document.querySelectorAll(`[helper-index="${chordHeadIndex}"]`);
-            if (!chordHeadDiv.length) continue;
-            chordHeadDiv = chordHeadDiv[0];
-
-            genHelperChordAndLevelDiv(chordHeadDiv, combineChordsString);
-            coloringCombineLines(chordHeadIndex, chordTailIndex);
-
-            //TODO: count
+            combineChordsSort.push(combineChords)
         }
 
+    }
+    combineChordsSort = combineChordsSort.sort((a, b) => b.length - a.length);
+
+    for (let combineChords of combineChordsSort) {
+        let combineChordLevelStr = genChordLevelCombineStr(combineChords, rootMap);
+        let combineChordsString = genChordCombineStr(combineChords, rootMap);
+        let chordHeadIndex = Number(combineChords[0].index);
+        let chordTailIndex = Number(combineChords[combineChords.length - 1].index);
+
+        let chordHeadDiv = document.querySelectorAll(`[helper-index="${chordHeadIndex}"]`);
+        if (!chordHeadDiv.length) continue;
+        chordHeadDiv = chordHeadDiv[0];
+
+        let genedLength = chordHeadDiv.getAttribute("gened");
+        genedLength = genedLength !== null ? Number(genedLength) : -1;
+        if (genedLength >= 0 && genedLength > combineChordsString.length) continue;
+
+        markCombineGened(chordHeadIndex, chordTailIndex, combineChordsString.length);
+
+
+        genHelperChordAndLevelDiv(chordHeadDiv, combineChordsString);
+
+        coloringCombineLines(chordHeadIndex, chordTailIndex);
     }
 
 }
 
-const coloringCombineLines = (headIndex, tailIndex) => {
-    console.log({ headIndex, tailIndex })
+const isOverlap = () => {
+
+}
+
+const markCombineGened = (headIndex, tailIndex, length) => {
+    // console.log({ headIndex, tailIndex, length });
+
     for (let i = headIndex; i <= tailIndex; i++) {
         let div = chordLyricDivs[i];
+        div.setAttribute("gened", length)
+    }
+}
+
+const coloringCombineLines = (headIndex, tailIndex) => {
+    // console.log({ headIndex, tailIndex });
+
+    for (let i = headIndex; i <= tailIndex; i++) {
+
+        let div = chordLyricDivs[i];
         div.style.backgroundColor = CONFIG.COMBINES.BG_COLOR;
+
+        let chordDiv = div.querySelector(".hopamchuan_chord");
+        if (chordDiv) {
+            chordDiv.style.color = "blue";
+        }
         // div.style.borderTop = "1px dashed #969696";
         // div.style.borderBottom = "1px dashed #969696";
     }
@@ -171,24 +222,23 @@ const lyricsAndChordsIndexer = () => {
 const getRootNote = () => {
     let selectedIndex = document.querySelector("#tool-box-trans-adj").selectedIndex;
     let rootNote = document.querySelectorAll("#tool-box-trans-adj option")[selectedIndex].getAttribute("data-key");
-    // root = root.replace(/maj7|m|7/g, "")
+    rootNote = rootNote.replace(/maj7|7/g, "")
     let isMinor = false;
     if (rootNote.includes("m")) isMinor = true;
 
     return { rootNote, isMinor }
 }
 
-
 const genChordLevelCombineStr = (combine, rootMap) => {
     return combine.map(i => {
-        let note = i.name.replace("m", "");
+        let note = i.name.replace(regRemoveColor, "");
         return rootMap.indexOf(note) + 1;
     }).join(",");;
 }
 
 const genChordCombineStr = (combine) => {
     return combine.map(i => {
-        let note = i.name.replace("m", "");
+        let note = i.name.replace(regRemoveColor, "");
         return note;
     }).join(",");;
 }
@@ -200,8 +250,8 @@ const genHelperNoteDiv = (text = "") => {
     noteDiv.classList.add('rhythm-item');
 
     noteDiv.style.border = "1px dashed #969696";
-    noteDiv.style.fontSize = "14px";
-    noteDiv.style.maxWidth = "120px";
+    noteDiv.style.fontSize = "13px";
+    // noteDiv.style.maxWidth = "120px";
     noteDiv.style.marginLeft = "5px";
     noteDiv.style.backgroundColor = "#fffce0";
 
@@ -225,7 +275,7 @@ const genLevel = () => {
     let keys = document.getElementsByClassName("hopamchuan_chord")
 
     for (let key of keys) {
-        let bac = rootChords.indexOf(key.innerText.replace("m", ""))
+        let bac = rootChords.indexOf(key.innerText.replace(regRemoveColor, ""))
         let bacDiv = `<div class="chord-level" style="color:blue;display: none;">|${bac + 1}</div>`
         key.innerHTML = `${key.innerText}${bacDiv}`
     }
@@ -244,18 +294,25 @@ const initShowLevel = () => {
 setTimeout(() => {
     enableGopDong();
     disableChordView();
+    disableChiaCot();
 }, 500)
 
 setTimeout(() => {
     chordLyricDivs = document.querySelectorAll(".hopamchuan_lyric, .hopamchuan_chord_inline");
     initAnalyze();
+
     initShowLevel();
     genLevel();
+    onShowLevel();
 
 }, 1000)
 
 const disableChordView = () => {
     document.getElementById('chord-view').style.opacity = 0;
+}
+
+const disableChiaCot = () => {
+    if (document.querySelector("#tool-box-column-toggle").classList.contains('on')) document.querySelector("#tool-box-column-toggle").click();
 }
 
 const enableGopDong = () => {
